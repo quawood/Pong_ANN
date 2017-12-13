dd98474dfe1f5d004629a71a936ae6f049a25de9
quawood <quawood1@gmail.com>
2017-12-13 14:55:56 -0500

    Functioning.

--- network_controller.py
+++ network_controller.py
@@ -4,7 +4,7 @@ from sigmoid import sigmoid, scalar
 import math
 from quadratic_cost import quadratic_cost, magnitude
 
-network = Network(layer_size=[2,2], activation_function=scalar, cost_function=quadratic_cost)
+network = Network(layer_size=[2,3,2], activation_function=scalar, cost_function=quadratic_cost)
 
 test1 = np.array([4478, 5]).reshape((1,2))
 data = np.loadtxt('ex1data1.txt', delimiter=',')
@@ -14,12 +14,14 @@ m = X1.shape[0]
 y1 = data[:, 2].reshape((m,1))/100000
 
 
-test2 = np.array([3]).reshape((1,1))
-X2 = np.array([[1],[2],[3],[4],[5]])
-y2 = np.array([[2],[4],[6],[8],[10]])
+test2 = np.array([2,2]).reshape((1,2))
+X2 = np.array([[1,1],[2,2],[3,3],[4,4],[5,5]])
+y2 = np.array([[2,2],[4,4],[6,6],[8,8],[10,10]])
 
 
-network.train(X1,y1, eta=0.0000001, max_iterations=100000)
+network.train(X2,y2, eta=0.01, max_iterations=10000)
+
+print(network.predict(test2))
 
 
 
