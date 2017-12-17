5354e7e1e182d22c4e93764330cc42dc397fa8a3
Qualan <quawood1@gmail.com>
2017-12-17 17:00:55 -0500

    generations write to file to save weights

--- GA/GA_controller.py
+++ GA/GA_controller.py
@@ -5,6 +5,8 @@ import numpy as np
 world = Environment()
 world.population_init(10)
 
+
+
 for g in range(0, 30):
     for i in range(0, len(world.population)):
         for j in range(0, len(world.population)):
@@ -21,27 +23,23 @@ for g in range(0, 30):
                         running = False
 
 
-    very_best = world.select(world.fitness(),1)
+    very_best = world.select(world.fitness,1)[0]
     W1 = very_best.ann.layers[0].W
     W2 = very_best.ann.layers[1].W
 
-    with open('best_weights', 'w') as outfile:
-        for data_slice in W1:
-            # The formatting string indicates that I'm writing out
-            # the values in left-justified columns 7 characters in width
-            # with 2 decimal places.
-            outfile.write('# Gen{0}\n'.format(g))
-            np.savetxt(outfile, data_slice, fmt='%-7.2f')
+    with open('best_weights.txt', 'wb') as outfile:
+        # The formatting string indicates that I'm writing out
+        # the values in left-justified columns 7 characters in width
+        # with 2 decimal places.
+        # outfile.write('# Gen1')
+        np.savetxt(outfile, W1, fmt='%-7.2f')
 
-            # Writing out a break to indicate different slices...
-
-    with open('best_weights2', 'w') as outfile:
-        for data_slice in W2:
-            # The formatting string indicates that I'm writing out
-            # the values in left-justified columns 7 characters in width
-            # with 2 decimal places.
-            outfile.write('# Gen{0}\n'.format(g))
-            np.savetxt(outfile, data_slice, fmt='%-7.2f')
+    with open('best_weights2.txt', 'wb') as outfile:
+        # The formatting string indicates that I'm writing out
+        # the values in left-justified columns 7 characters in width
+        # with 2 decimal places.
+        # outfile.write('# Gen1')
+        np.savetxt(outfile, W2, fmt='%-7.2f')
 
     world.new_generation(4)
 
